75bc82a438322463e5065b0cefccf67d4e94e264
Marcos-Pietrucci <marcospietrucci@gmail.com>
2019-05-24 00:00:45 -0300

    Adicionado interface crua, com colisão, e limite de movimento

--- projeto-jogo/main.cpp
+++ projeto-jogo/main.cpp
@@ -1,7 +1,11 @@
 #include<stdio.h>
 #include<string.h>
 #include<SDL.h>
+#include"SDL_opengl.h"
+#include<time.h>
 // PARA ARQUIVOS BINARIOS:
+
+#define NO_STDIO_REDIRECT
 struct usuario{
 
     char nome[50];
@@ -10,9 +14,18 @@ struct usuario{
     int derrotas;
 };
 
+struct movimento{
+
+    bool esq = false;
+    bool dir = false;
+    bool up = false;
+    bool down = false;
+    bool shot = false;
+};
+
 FILE *arq;
-bool arquivo = false; //!CUIDADO AO MANIPULAR ESTA VARIÁVEL
-//Indica se o arquivo esta aberto
+bool arquivo = false; //Indica se o arquivo esta aberto
+bool acabou_jogo = false; //Indica se o jogo acabou ou não
 struct usuario Jogador1;
 struct usuario Jogador2;
 
@@ -125,13 +138,344 @@ void salva_dados_DEV(struct usuario *alvo,char nome[50], int b,int d, int e) //f
     alvo->vitorias = d;
     alvo->derrotas = e;
 }
+void configurar()
+{
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    //memoria
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8); //transparencia
+    SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16); //3D - não sei se vamos usar
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1); //3D - não sei se vamos usar
+
+    //Definir nome para nossa janela
+    SDL_WM_SetCaption("Naruto Simulator!", NULL);
+
+    //Tamanho da janela - (x,y, bits/pixel, tipo_janela)
+    SDL_SetVideoMode(800,600, 32,SDL_OPENGL);
+
+    // Cor da janea
+    glClearColor(4,5,2,1);
+
+
+    //Area exibida
+    glViewport(0,0,800,600);
+
+    //Define sombra
+    glShadeModel(GL_SMOOTH);
+
+    //2D (COOL)
+    glMatrixMode(GL_PROJECTION); //Aplica a projeção da matriz atual
+    glLoadIdentity();
+
+    glDisable(GL_DEPTH_TEST);
+
+}
+time_t Tick[2];
+void movimento(struct movimento *J1, struct movimento *J2, float *J1X, float *J1Y, float *J2X, float *J2Y,
+                float **tiros1, float **tiros2, float J1Comp, float J1Alt, float J2Comp, float J2Alt) //Processa todos os movimentos necessários, e retorna os valores das coordernadas
+{
+    //!LIMITAR OS PERSONAGENS NA TELA
+    //!LIMITAR PROJETEIS NA TELA
+    int i;
+
+    if(J1->esq && *J1X > 0) //O ponto inicial dele
+        *J1X -= 0.1;
+
+    if(J1->dir && *J1X + J1Comp < 400) //Trava o movimento do personagem no quadrado estabelecido
+        *J1X += 0.1;
+
+    if(J2->esq && *J2X > 400) //se a tecla A for pressionada
+        *J2X -= 0.1;
+
+    if(J2->dir && *J2X + J2Comp < 800) //se a tecla D for pressionada
+        *J2X += 0.1;
+
+    if(J1->up && *J1Y > 0)
+        *J1Y -= 0.1;
+
+    if(J1->down && *J1Y + J1Alt < 600)
+        *J1Y += 0.1;
+
+    if(J2->up && *J2Y > 0)
+        *J2Y -= 0.1;
+
+    if(J2->down && *J2Y + J2Alt < 600)
+        *J2Y += 0.1;
+
+    if(J1->shot)
+    {
+        J1->shot = false;
+        for(i = 0; i < 100 ; i++)
+        {
+            if(!tiros1[i][2]) //Se a terceira coluna de uma das linhas de tiro estiver "Vaga" (for igual a zero)
+            {
+                tiros1[i][0] =  *J1X; //O tiro começa na mesma posição do personagem
+                tiros1[i][1] =  *J1Y;
+                tiros1[i][2] = 1.0; //indica que ha esse tiro
+                i = 100;
+            }
+        }
+    }
+
+    if(J2->shot)
+    {
+        J2->shot = false;
+        for(i = 0; i < 100; i++)
+        {
+            if(!tiros2[i][2])
+            {
+                tiros2[i][0] = *J2X;
+                tiros2[i][1] = *J2Y;
+                tiros2[i][2] = 1.0; //indica que há esse tiro
+                i = 100;
+            }
+        }
+
+    }
+
+}
+
 int main(int argc, char* args[])
 {
+    float **tiros1; //A primeira coluna é a posição X, a segunda Y, a terceira indica se aquele tiro ainda existe
+    float **tiros2; //A primeira coluna é a posição X, a segunda Y, a terceira indica se aquele tiro ainda existe
+    int i;
+
+    tiros1 = (float **) calloc(100, sizeof(float *));
+    tiros2 = (float **) calloc(100, sizeof(float *));
+
+    for(i = 0; i < 100; i++)
+    {
+        tiros1[i] = (float *) calloc(3, sizeof(float));
+        tiros2[i] = (float *) calloc(3, sizeof(float));
+    }
+
+    Tick[0] = Tick[1] = time(NULL);
+
     arquivo = false;
     /*scanf("%50[^\n]s", nome_inicial);*/
-    //OBS: quando eu falo "informações do jogador" quero dizer tudo que esta na struct usuario chamada Jogador
-    abrir_arq("ab+");
+    configurar();
 
+    //!------------ LOGICA AQUI EM BAIXO ---------------------
+    bool executando = true;
+
+    //Varial que detecta clickes de teclas
+    SDL_Event eventos;
+
+    //Iniciando variaveis para elementos e personagens
+    float J1X = 50.0;
+    float J1Y = 300.0;
+    float J1Comp = 50.0;
+    float J1Alt = 30.0;
+
+    float TiroComp  = 30;
+    float TiroLarg = 20;
+
+
+    float J2X = 750.0;
+    float J2Y = 300.0;
+    float J2Comp = J1Comp;
+    float J2Alt = J1Alt;
+
+    //Variáveis para movimentar o personagem
+    struct movimento J1;
+    struct movimento J2;
+
+    //loop do jogo
+    while(executando)
+    {
+
+        //eventos
+        while(SDL_PollEvent(&eventos))
+        {
+            //Fecha com o X da janela
+            if(eventos.type == SDL_QUIT || acabou_jogo)
+                executando = false;
+
+            //Detectar movimento
+
+            if(eventos.type == SDL_KEYDOWN) //Se uma tecla foi pressionada!!
+            { //enquanto a tecla esta pressionada, movimentar
+                if(eventos.key.keysym.sym == SDLK_LEFT) //Apertou seta esquerda
+                    J1.esq = true;
+
+                if(eventos.key.keysym.sym == SDLK_RIGHT) //Apertou seta direita
+                    J1.dir = true;
+
+                if(eventos.key.keysym.sym == SDLK_a)
+                    J2.esq = true;
+
+                if(eventos.key.keysym.sym == SDLK_d)
+                    J2.dir = true;
+
+                if(eventos.key.keysym.sym == SDLK_UP)
+                    J1.up = true;
+
+                if(eventos.key.keysym.sym == SDLK_DOWN)
+                    J1.down = true;
+
+                if(eventos.key.keysym.sym == SDLK_w)
+                    J2.up = true;
+
+                if(eventos.key.keysym.sym == SDLK_s)
+                    J2.down = true;
+
+                if(eventos.key.keysym.sym == SDLK_SLASH)
+                {
+                    time_t tempo = time(NULL);
+                    if((double) (tempo - Tick[0]) >= 1.5) //A diferença entre o tiro anterior e esse tem que ser maior que 1s
+                    {
+                        Tick[0] = tempo;
+                        J1.shot = true;
+                    }
+                }
+                if(eventos.key.keysym.sym == SDLK_SPACE)
+                {
+
+                    time_t tempo = time(NULL);
+                    if((double) (tempo - Tick[1]) >= 1.5) //A diferença entre o tiro anterior e esse tem que ser maior que 1s
+                    {
+                        Tick[1] = tempo;
+                        J2.shot = true;
+                    }
+                }
+            }
+
+            else if(eventos.type == SDL_KEYUP)
+            { //se a tecla foi solta, para de se mover
+                if(eventos.key.keysym.sym == SDLK_LEFT) //Apertou seta esquerda
+                    J1.esq = false;
+
+                if(eventos.key.keysym.sym == SDLK_RIGHT) //Apertou seta direita
+                    J1.dir = false;
+
+                if(eventos.key.keysym.sym == SDLK_a)
+                    J2.esq = false;
+
+                if(eventos.key.keysym.sym == SDLK_d)
+                    J2.dir = false;
+
+                if(eventos.key.keysym.sym == SDLK_UP)
+                    J1.up = false;
+
+                if(eventos.key.keysym.sym == SDLK_DOWN)
+                    J1.down = false;
+
+                if(eventos.key.keysym.sym == SDLK_w)
+                    J2.up = false;
+
+                if(eventos.key.keysym.sym == SDLK_s)
+                    J2.down = false;
+
+            }
+        }
+
+
+        //!LOGICA DO PROGRAMA -- movimento do personagem
+        movimento(&J1, &J2, &J1X, &J1Y, &J2X, &J2Y, tiros1, tiros2, J1Comp, J1Alt, J2Comp, J2Alt); //Processa todo o movimento necessário
+
+        //!!RENDERIZAÇÃO!!
+        glClear(GL_COLOR_BUFFER_BIT); //limpa o buffer
+
+        //Inicia Matriz
+        glPushMatrix();
+
+        //Configura a matriz
+        glOrtho(0, 800, 600, 0, -1, 1);
+
+        //COR
+         //vermelho
+
+        //inicia desenho
+        glBegin(GL_QUADS); //GL_POINTS, GL_LINES, GL_LINES_LOOP, GL_TRIANGLE, GL_POLIGON
+
+            glColor4ub(255,0,0, 255);
+            //Desenhando jogador1
+            glVertex2f(J1X, J1Y); //primeiro ponto
+            glVertex2f(J1X + J1Comp, J1Y); //segundo ponto
+            glVertex2f(J1X + J1Comp, J1Y + J1Alt);
+            glVertex2f(J1X, J1Y + J1Alt);
+
+            //Desenhando Jogador2
+            glColor4ub(0,0,255,255);
+            glVertex2f(J2X, J2Y); //primeiro ponto
+            glVertex2f(J2X + J2Comp, J2Y); //segundo ponto
+            glVertex2f(J2X + J2Comp, J2Y + J2Alt);
+            glVertex2f(J2X, J2Y + J2Alt);
+
+        for(i = 0; i < 100; i++) //Busca nas duas matrizes de tiro por projeteis que tenham sido atirados
+        { //!JOGADOR 1 à esquerda
+        //!Jogador 2 à direita
+        //!DENHA E FAZ MOVIMENTAR OS PROJETEIS
+
+            //Atualiza as posições dos tiros
+            if(tiros1[i][2] != 0.0) // Coordenadas do tiro são (tiro1[i][0] ; tiros1[i][1])
+            {
+                if(tiros1[i][0] + TiroComp> 800) //Se saiu da tela
+                    tiros1[i][2] = 0.0; //Mata o bloco
+                //Desenha - atualiza a posição dos tiros
+
+                //Ve se teve colisão
+                if(tiros1[i][0] >= J2X- J2Comp + 15 && tiros1[i][1] >= J2Y - 20 && tiros1[i][1] <= J2Y + J2Alt)// Some com o tiro, computa o dano
+                    tiros1[i][2] = 0.0;   //Esses números somando são para calibrar a hitbox
+                else{
+                glColor4ub(123,222,87, 255);
+                glVertex2f(tiros1[i][0], tiros1[i][1]); //primeiro ponto
+                glVertex2f(tiros1[i][0] + TiroComp, tiros1[i][1]); //segundo ponto
+                glVertex2f(tiros1[i][0] + TiroComp, tiros1[i][1] + TiroLarg);
+                glVertex2f(tiros1[i][0], tiros1[i][1] + TiroLarg);
+
+                tiros1[i][0] += 0.05; //Atualiza a posição do tiro
+                }
+            }
+            if(tiros2[i][2] != 0.0)
+            {
+                if(tiros2[i][0] < 0)
+                    tiros2[i][2] = 0.0;
+                //Detectar colisão com os personagens, projetil por projetil!!!!
+
+                //Ve se teve colisão
+                if(tiros2[i][0] <= J1X + J1Comp + 10 && tiros2[i][1] >= J1Y - 20 && tiros2[i][1] <= J1Y + J1Alt)// Some com o tiro, computa o dano
+                    tiros2[i][2] = 0.0;
+                else
+                {
+                    glColor4ub(0,0,0, 255);
+                    glVertex2f(tiros2[i][0], tiros2[i][1]); //primeiro ponto
+                    glVertex2f(tiros2[i][0] + TiroComp, tiros2[i][1]); //segundo ponto
+                    glVertex2f(tiros2[i][0] + TiroComp, tiros2[i][1] + TiroLarg);
+                    glVertex2f(tiros2[i][0], tiros2[i][1] + TiroLarg);
+                    tiros2[i][0] -= 0.05;
+                }
+
+            }
+        }
+
+
+
+        glEnd();
+        //Fecha matriz
+        glPopMatrix();
+
+        //Animação -- precisa definir mas é meio inutil
+        SDL_GL_SwapBuffers();
+
+    }
+
+    //para detectar uma tecla "if(eventos.key.keysym.sym == SDLK_ESCAPE)"
+
+
+    for(i = 0; i < 100; i++)
+    {
+        free(tiros1[i]);
+        free(tiros2[i]);
+    }
+    free(tiros1);
+    free(tiros2);
+    SDL_Quit();
 
     if(arquivo)
         fclose(arq);
